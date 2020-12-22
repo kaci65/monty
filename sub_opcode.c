629e6a4ee04a055659a6d09f22ce1901386c7f56
david-kimengu <davidmutune200@gmail.com>
2020-12-22 08:46:50 +0000

    Display right error msg if sub fails

--- sub_opcode.c
+++ sub_opcode.c
@@ -2,14 +2,14 @@
 /**
  * sub_opcode - subtracts the top element of the stack from 2nd top element.
  * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
+ * @line_num: Interger representing the line number of of the opcode.
  */
-void sub_opcode(stack_t **stack, unsigned int line_number)
+void sub_opcode(stack_t **stack, unsigned int line_num)
 {
 	int quotient;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		error_check(8, line_number, "add");
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_num);
 
 	(*stack) = (*stack)->next;
 	quotient = (*stack)->n - (*stack)->prev->n;
