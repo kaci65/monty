1b95ec8d29f38e444084c4b650b593f4a87374bb
david-kimengu <davidmutune200@gmail.com>
2020-12-22 06:06:18 +0000

    Add nop opcode functionality

---
+++ nop_opcode.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of the opcode.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	stack = stack;
+	line_number = line_number;
+}
