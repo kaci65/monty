f33bb6c9db4aab8adff380737a7cf412918b6876
david-kimengu <davidmutune200@gmail.com>
2020-12-22 05:12:46 +0000

    write error to stderr

--- print_top.c
+++ print_top.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "monty.h"
 
 /**
@@ -11,7 +10,7 @@ void print_top(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
