986b3ed08512ea6d5810410cc1ec13a82bcd17fe
david-kimengu <davidmutune200@gmail.com>
2020-12-22 05:05:25 +0000

    edit null condition check

--- print_top.c
+++ print_top.c
@@ -9,7 +9,7 @@
  */
 void print_top(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
