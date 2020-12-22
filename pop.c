273dbf8c1fcca3858fcc9404cf1c4178a3359295
david-kimengu <davidmutune200@gmail.com>
2020-12-22 04:48:52 +0000

    Remove compilation error

--- pop.c
+++ pop.c
@@ -8,15 +8,16 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t top_node;
+	stack_t *top_node;
 
-	if (!*stack)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop ab empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	top_node = *stack;
 	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
 	free(top_node);
 }
