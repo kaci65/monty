1f58d2d848b076a745c9b286fa5565fcbc33472f
kaci65 <wanjikukarugi@gmail.com>
2020-12-27 12:25:54 +0300

    Add NULL check

--- pop.c
+++ pop.c
@@ -10,13 +10,16 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top_elem;
 
-	if (!(*stack || !stack))
+	if (!(*stack) || !stack)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	top_elem = *stack;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
+	*stack = top_elem->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
 	free(top_elem);
 }
